7d7a07392a57ff9a7f95f05c4e5ae1e154107654
Magle-corp <wolff.kevin@protonmail.com>
2023-03-15 01:10:15 +0100

    Add Primeflex

--- next/src/pages/_app.tsx
+++ next/src/pages/_app.tsx
@@ -1,7 +1,8 @@
 import type { AppProps } from 'next/app';
-import 'primereact/resources/themes/mdc-light-indigo/theme.css';
+import 'primereact/resources/themes/lara-light-blue/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
+import '/node_modules/primeflex/primeflex.css';
 import { GlobalStyle, ThemeWrapper } from '@/theme';
 
 export default function App({ Component, pageProps }: AppProps) {
