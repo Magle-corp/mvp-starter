595d448e971707016644e04a93a2d369719b51d2
Magle-corp <wolff.kevin@protonmail.com>
2023-03-19 22:24:46 +0100

    Add PrimeReact locale FR

--- next/src/pages/_app.tsx
+++ next/src/pages/_app.tsx
@@ -1,10 +1,15 @@
 import type { AppProps } from 'next/app';
+import { addLocale, locale } from 'primereact/api';
+import primeLocaleFr from '@/cdn/i18n/primeLocaleFr.json';
 import 'primereact/resources/themes/lara-light-blue/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
 import { GlobalStyle, ThemeWrapper } from '@/theme';
 
 export default function App({ Component, pageProps }: AppProps) {
+  addLocale('fr', primeLocaleFr);
+  locale('fr');
+
   return (
     <ThemeWrapper>
       <GlobalStyle />
