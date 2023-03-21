15d2fc8d0ba762271a02ff6f2f196b405dc16908
Magle-corp <wolff.kevin@protonmail.com>
2023-03-21 23:21:31 +0100

    Add React Query

--- next/src/pages/_app.tsx
+++ next/src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { addLocale, locale } from 'primereact/api';
 import primeLocaleFr from '@/cdn/i18n/primeLocaleFr.json';
 import 'primereact/resources/themes/lara-light-blue/theme.css';
@@ -10,10 +11,14 @@ export default function App({ Component, pageProps }: AppProps) {
   addLocale('fr', primeLocaleFr);
   locale('fr');
 
+  const queryClient = new QueryClient();
+
   return (
-    <ThemeWrapper>
-      <GlobalStyle />
-      <Component {...pageProps} />
-    </ThemeWrapper>
+    <QueryClientProvider client={queryClient}>
+      <ThemeWrapper>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </ThemeWrapper>
+    </QueryClientProvider>
   );
 }
