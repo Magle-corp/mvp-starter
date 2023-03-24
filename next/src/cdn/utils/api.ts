9fb352aec9383e83eb767273941e1756987059ee
Magle-corp <wolff.kevin@protonmail.com>
2023-03-24 23:44:44 +0100

    Add Next base api url env variable

--- next/src/cdn/utils/api.ts
+++ next/src/cdn/utils/api.ts
@@ -1,11 +1,11 @@
 import axios, { AxiosInstance } from 'axios';
 
 const api: AxiosInstance = axios.create({
-  baseURL: process.env.BASE_URL,
+  baseURL: process.env.API_BASE_URL,
   timeout: 4000,
   headers: {
-    'Content-type': 'application/json',
-    Accept: 'application/json',
+    'Content-type': 'application/ld+json',
+    Accept: 'application/ld+json',
   },
 });
 
