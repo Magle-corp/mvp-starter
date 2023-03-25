99e445104e1c7490d4121cd0cadda79f9a02e293
Magle-corp <wolff.kevin@protonmail.com>
2023-03-25 21:49:05 +0100

    Add api error handling for signIn form

--- symfony/src/EventSubscriber/ExceptionManager.php
+++ symfony/src/EventSubscriber/ExceptionManager.php
@@ -6,6 +6,8 @@ use App\Exception\ApiExceptionCustom409;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Security\Core\Exception\BadCredentialsException;
+use Symfony\Component\Security\Http\Event\LoginFailureEvent;
 
 final class ExceptionManager implements EventSubscriberInterface
 {
@@ -13,20 +15,30 @@ final class ExceptionManager implements EventSubscriberInterface
     public static function getSubscribedEvents(): array
     {
         return [
-            KernelEvents::EXCEPTION => ['customException'],
+            KernelEvents::EXCEPTION => ['customKernelEventExceptions'],
+            LoginFailureEvent::class => ['customLoginFailureEventException']
         ];
     }
 
-    public function customException(ExceptionEvent $event)
+    public function customKernelEventExceptions(ExceptionEvent $event)
     {
-        $currentExceptionThrowable = $event->getThrowable();
-        $exceptionThrowableClass = get_class($currentExceptionThrowable);
+        $exceptionThrowable = $event->getThrowable();
 
-        if (!$currentExceptionThrowable instanceof ApiExceptionCustom409)
+        if (!$exceptionThrowable instanceof ApiExceptionCustom409)
         {
+            $exceptionThrowableClass = get_class($exceptionThrowable);
             $event->setThrowable(new $exceptionThrowableClass(
                 'Un problème technique est survenu, veuillez réessayer ultérieurement'
             ));
+        }
+    }
+
+    public function customLoginFailureEventException(LoginFailureEvent $event)
+    {
+        if ($event->getException() instanceof BadCredentialsException)
+        {
+            $customResponse = $event->getResponse()->setContent('{"code":401,"message":"Identifiants invalides"}');
+            $event->setResponse($customResponse);
         }
     }
 }
