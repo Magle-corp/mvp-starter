804dd2de6638332e4a72383c5fc8a3a8c67b5d0a
Magle-corp <wolff.kevin@protonmail.com>
2023-03-28 23:42:05 +0200

    Add ResponseService

---
+++ symfony/src/Service/ResponseService.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class ResponseService
+{
+    public function create(string $message, int $code): Response
+    {
+        return new Response(
+            '{"message":"' . $message . '"}',
+            $code
+        );
+    }
+
+    public function error()
+    {
+        return $this->create('Un problème technique est survenu, veuillez réessayer ultérieurement', 500);
+    }
+}
