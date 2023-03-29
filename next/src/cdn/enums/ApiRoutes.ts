b851720cca4e39b7bd37334b25eb778701048fd0
Magle-corp <wolff.kevin@protonmail.com>
2023-03-29 17:59:07 +0200

    Add frontend resend signUp validation email system

--- next/src/cdn/enums/ApiRoutes.ts
+++ next/src/cdn/enums/ApiRoutes.ts
@@ -2,6 +2,7 @@ enum ApiRoutes {
   USER = '/users',
   SIGN_IN = '/signIn',
   SIGN_UP_VALIDATION = '/signUpValidation',
+  SIGN_UP_VALIDATION_EMAIL = '/signUpValidationEmail',
 }
 
 export default ApiRoutes;
