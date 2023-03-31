526c304136aebdea38bd7124d1df511184d4472f
Magle-corp <wolff.kevin@protonmail.com>
2023-03-31 23:58:26 +0200

    Add backend api for reset password

---
+++ symfony/src/Controller/ResetPasswordController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Service\EmailService;
+use App\Service\JWTService;
+use App\Service\ResponseService;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Attribute\AsController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+#[AsController]
+class ResetPasswordController extends AbstractController
+{
+    private JWTService $JWTService;
+    private ResponseService $responseService;
+    private EntityManagerInterface $entityManager;
+    private UserPasswordHasherInterface $passwordHasher;
+    private EmailService $emailService;
+
+    public function __construct(
+        JWTService                  $JWTService,
+        ResponseService             $responseService,
+        EntityManagerInterface      $entityManager,
+        UserPasswordHasherInterface $passwordHasher,
+        EmailService                $emailService
+    )
+    {
+        $this->JWTService = $JWTService;
+        $this->responseService = $responseService;
+        $this->entityManager = $entityManager;
+        $this->passwordHasher = $passwordHasher;
+        $this->emailService = $emailService;
+    }
+
+    public function __invoke(Request $request)
+    {
+        $requestContent = json_decode($request->getContent(), true);
+
+        if (!array_key_exists('token', $requestContent) || !array_key_exists('password', $requestContent)) {
+            return $this->responseService->error();
+        }
+
+        $resetPasswordToken = $requestContent['token'];
+        $newPassword = $requestContent['password'];
+
+        $isExpiredToken = $this->JWTService->isExpired($resetPasswordToken);
+        $isValidToken = $this->JWTService->isValid($resetPasswordToken);
+        $isValidSecret = $this->JWTService->check($resetPasswordToken, getenv('JWT_SIGNUP_SECRET'));
+
+        if ($isExpiredToken) {
+            return $this->responseService->create('Le token n\'est plus valide', 401);
+        }
+
+        if (!$isValidToken || !$isValidSecret) {
+            return $this->responseService->create('Le token n\'est pas valide', 409);
+        }
+
+        $tokenPayload = $this->JWTService->getPayload($resetPasswordToken);
+
+        if (!array_key_exists('user_id', $tokenPayload)) {
+            return $this->responseService->error();
+        }
+
+        $userId = $tokenPayload['user_id'];
+
+        $userRepository = $this->entityManager->getRepository(User::class);
+        $user = $userRepository->findOneBy(['id' => $userId]);
+
+        if (!$user) {
+            return $this->responseService->create('Aucun compte correspondant', 409);
+        }
+
+        $user->setPassword($this->passwordHasher->hashPassword($user, $newPassword));
+
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+
+        return $this->responseService->create('OK', 200);
+    }
+}
