a4787b3697a05bd4de275a9cd2c4d939efe59c9b
Magle-corp <wolff.kevin@protonmail.com>
2023-04-01 17:51:07 +0200

    Rename refresh token entity and update use

--- symfony/src/Entity/TokenRefresh.php
+++ symfony/src/Entity/TokenRefresh.php
@@ -6,7 +6,7 @@ use Doctrine\ORM\Mapping as ORM;
 use Gesdinet\JWTRefreshTokenBundle\Entity\RefreshToken as BaseRefreshToken;
 
 #[ORM\Entity]
-#[ORM\Table("refresh_tokens")]
-class RefreshToken extends BaseRefreshToken
+#[ORM\Table("tokens_refresh")]
+class TokenRefresh extends BaseRefreshToken
 {
 }
