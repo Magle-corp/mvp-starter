1c6484cc3d9c254b18d3e90b5c4f22f8a3f6334b
Magle-corp <wolff.kevin@protonmail.com>
2023-04-14 21:46:19 +0200

    Handle signin success when user account is not verified

--- symfony/src/EventSubscriber/AuthenticationSubscriber.php
+++ symfony/src/EventSubscriber/AuthenticationSubscriber.php
@@ -2,24 +2,40 @@
 
 namespace App\EventSubscriber;
 
+use App\Service\ResponseService;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\Security\Core\Exception\BadCredentialsException;
 use Symfony\Component\Security\Http\Event\LoginFailureEvent;
+use Symfony\Component\Security\Http\Event\LoginSuccessEvent;
 
 final class AuthenticationSubscriber implements EventSubscriberInterface
 {
+    private ResponseService $responseService;
+
+    public function __construct(ResponseService $responseService)
+    {
+        $this->responseService = $responseService;
+    }
+
     public static function getSubscribedEvents(): array
     {
         return [
-            LoginFailureEvent::class => ['customLoginFailureEventException']
+            LoginFailureEvent::class => ['customLoginFailureEventException'],
+            LoginSuccessEvent::class => ['customLoginSuccessEventException']
         ];
     }
 
     public function customLoginFailureEventException(LoginFailureEvent $event): void
     {
         if ($event->getException() instanceof BadCredentialsException) {
-            $customResponse = $event->getResponse()->setContent('{"message":"Identifiants invalides"}');
-            $event->setResponse($customResponse);
+            $event->setResponse($this->responseService->create('Identifiants invalides', 401));
+        }
+    }
+
+    public function customLoginSuccessEventException(LoginSuccessEvent $event): void
+    {
+        if (!$event->getAuthenticatedToken()->getUser()->isVerified()) {
+            $event->setResponse($this->responseService->create("Veuillez confirmer votre inscription pour pouvoir vous connecter", 403));
         }
     }
 }
