31a9f1c5aceb8945426333581a47cb44395951ee
Magle-corp <wolff.kevin@protonmail.com>
2023-04-22 15:23:02 +0200

    Update UI Card

--- next/src/features/authentication/components/AuthCard.tsx
+++ next/src/features/authentication/components/AuthCard.tsx
@@ -1,40 +1,7 @@
-import { ReactNode } from 'react';
 import styled from 'styled-components';
 import Card from '@/ui/atoms/Card';
 
-type AuthCard = {
-  children: ReactNode;
-  title: string;
-};
-
-const AuthCard = (props: AuthCard) => {
-  return (
-    <StyledCard>
-      <Section>
-        <Title>{props.title}</Title>
-        {props.children}
-      </Section>
-    </StyledCard>
-  );
-};
-
-const StyledCard = styled(Card)`
+export default styled(Card)`
   max-width: 360px;
   margin: 3rem auto 0;
 `;
-
-const Section = styled.section`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 4rem;
-`;
-
-const Title = styled.h1`
-  font-size: 2.5rem;
-  font-weight: 700;
-  line-height: 1.25;
-  text-align: center;
-`;
-
-export default AuthCard;
--- next/src/ui/atoms/Card.tsx
+++ next/src/ui/atoms/Card.tsx
@@ -1,8 +1,41 @@
 import styled from 'styled-components';
-import { Card } from 'primereact/card';
+import { Card as PCard, CardProps } from 'primereact/card';
+import { ReactNode } from 'react';
 
-export default styled(Card)`
+type Card = {
+  title?: string;
+  children: ReactNode;
+} & CardProps;
+
+const Card = (props: Card) => {
+  return (
+    <StyledCard className={props.className}>
+      <Section>
+        {props.title && <Title>{props.title}</Title>}
+        {props.children}
+      </Section>
+    </StyledCard>
+  );
+};
+
+const StyledCard = styled(PCard)`
   width: 100%;
   height: max-content;
   border-radius: 3px;
 `;
+
+const Section = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 4rem;
+`;
+
+const Title = styled.h1`
+  font-size: 2.5rem;
+  font-weight: 700;
+  line-height: 1.25;
+  text-align: center;
+`;
+
+export default Card;
