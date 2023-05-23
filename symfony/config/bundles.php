a1a12df01d58d66cb5eca996779691436fd1e82b
Magle-corp <wolff.kevin@protonmail.com>
2023-05-23 22:12:08 +0200

    Add Vich uploader

--- symfony/config/bundles.php
+++ symfony/config/bundles.php
@@ -18,4 +18,5 @@ return [
     Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
+    Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
 ];
